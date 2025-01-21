7508cf02b20553ee8ffe2c1c56f44b923e3fea26
Jędrzej <mr.myszer@gmail.com>
2025-01-21 15:39:33 +0100

    pushed project main

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include<map>
+#include<utility>
+#include<set>
+#include <fstream>
+#include <sstream>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdexcept>
+#include<initializer_list>
+#include<Uzytkownicy.h>
+#include<Baza.h>
+
+int main()
+{
+	Baza b;
+	b.operacja();
+}
